33a4e19469637801ecd468eafd74fe670a825e48
lm <super1742720898@gmail.com>
2021-03-22 15:46:41 +0800

    leet-code 206反转链表

--- linked-list/src/com/github/aliez/Solution206.java
+++ linked-list/src/com/github/aliez/Solution206.java
@@ -7,4 +7,34 @@ package com.github.aliez;
  * @date 2021/3/22 15:25
  */
 public class Solution206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre =null;
+        while (cur!=null){
+            ListNode temp =cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur =temp;
+        }
+        return pre;
+
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        ListNode cur =head;
+        ListNode pre=null;
+
+        return reverse(cur,pre);
+
+    }
+    public ListNode reverse(ListNode cur,ListNode pre){
+        if (cur==null){
+            return pre;
+        }
+        ListNode temp=cur.next;
+        cur.next=pre;
+        pre=cur;
+        cur=temp;
+      return   reverse(cur,pre);
+    }
 }
