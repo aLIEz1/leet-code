01ef2ed67927d74757cb5a6079ab74ef28e2b0d2
lm <super1742720898@gmail.com>
2021-03-26 18:01:06 +0800

    leet-code 18 四数之和

--- hash-table/src/com/github/aliez/Solution18.java
+++ hash-table/src/com/github/aliez/Solution18.java
@@ -1,5 +1,7 @@
 package com.github.aliez;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,6 +12,39 @@ import java.util.List;
  */
 public class Solution18 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int k = 0; k < nums.length; k++) {
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
+            for (int i = k + 1; i < nums.length; i++) {
+                if (i > k + 1 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                int left = i + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    if (nums[k] + nums[i] + nums[left] + nums[right] > target) {
+                        right--;
+                    } else if (nums[k] + nums[i] + nums[left] + nums[right] < target) {
+                        left++;
+                    } else {
+                        List<Integer> integers = Arrays.asList(nums[k], nums[i], nums[left], nums[right]);
+                        ans.add(integers);
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        right--;
+                        left++;
+                    }
+                }
+            }
+        }
+        return ans;
 
     }
 }
