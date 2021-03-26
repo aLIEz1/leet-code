a1eca751d9603f24ded9fde7bfc37dd86c37504a
lm <super1742720898@gmail.com>
2021-03-26 17:00:04 +0800

    leet-code 454 三数之和

--- hash-table/src/com/github/aliez/Solution15.java
+++ hash-table/src/com/github/aliez/Solution15.java
@@ -1,5 +1,9 @@
 package com.github.aliez;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 15
  *
@@ -7,4 +11,47 @@ package com.github.aliez;
  * @date 2021/3/26 15:35
  */
 public class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length == 0) {
+            return ans;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return ans;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    List<Integer> integers = Arrays.asList(nums[i], nums[left], nums[right]);
+                    ans.add(integers);
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        Solution15 solution15 = new Solution15();
+        System.out.println(solution15.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+
+    }
 }
---
+++ hash-table/src/com/github/aliez/Solution18.java
@@ -0,0 +1,15 @@
+package com.github.aliez;
+
+import java.util.List;
+
+/**
+ * 18
+ *
+ * @author lm
+ * @date 2021/3/26 16:58
+ */
+public class Solution18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+    }
+}
