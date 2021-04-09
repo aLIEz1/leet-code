6c45d2a6c0549c21fa3c197e16faaadf957b18fc
lm <super1742720898@gmail.com>
2021-04-09 13:56:20 +0800

    leet-code 617 合并二叉树

---
+++ binary-tree/src/com/github/aliez/Solution617.java
@@ -0,0 +1,22 @@
+package com.github.aliez;
+
+/**
+ * 617 合并二叉树
+ *
+ * @author lm
+ * @date 2021/4/9 13:20
+ */
+public class Solution617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+}
